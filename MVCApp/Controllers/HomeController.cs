8d70094e57211e72edf34c003b13f35f22752f09
dororite <balakrishnan2k00@gmail.com>
2026-02-04 00:00:48 +0530

    lazy loading

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace MVCApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private ApplicationDBContext _dbContext;
+        private ApplicationDBContext _dbContext; // need to create to access db
         public HomeController(ILogger<HomeController> logger, ApplicationDBContext dbContext)
         {
             _logger = logger;
@@ -16,8 +16,29 @@ namespace MVCApp.Controllers
 
         public IActionResult Index()
         {
+            //lazy loading - default
+                IEnumerable<Villa> villas1 = _dbContext.villas;
+
+                List<Villa> villas = _dbContext.villas.ToList();
 
 
+                //for loop
+                    //var totalVilla = villas.Count();
+
+                    ////Console.WriteLine(totalVilla);
+
+                    //for (int i = 0; i<totalVilla; i++)
+                    //{
+                    //    villas[i].VillaAmenity = _dbContext.villaAmenities.Where(u => u.VillaId == villas[i].Id).ToList();
+                    //}
+                //foreach loop
+            
+                foreach(var villa in villas)
+                {
+                    villa.VillaAmenity = _dbContext.villaAmenities.Where(u => u.VillaId == villa.Id).ToList();
+                }
+
+            //Eager Loading
 
 
 
